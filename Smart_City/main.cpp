fca5ccdaf42a733e226fbb162b48a167d94611ba
Omar Niazi <niaziomar609@gmail.com>
2025-11-30 20:42:26 +0500

    Added some Templated Data Structures. Namespace re.

--- Smart_City/main.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-	cout << "Welcome to the Smart City Management System!" << endl;
-	return 0;
-}
---
+++ Smart_City/re/custom_STL.h
@@ -0,0 +1,9 @@
+#pragma once
+// Wrapper header to include custom STL implementations
+#include "Vector.h"
+#include "LinkedLists.h"
+#include "BST.h"
+#include "PriorityQueue.h"
+#include "Stack.h"
+#include "Queue.h"
+#include "NaryTree.h"
